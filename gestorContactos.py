50435cb9d5a168566221e1547e09adef464fec95
Martin Martinez <investingmtz@gmail.com>
2021-02-06 11:11:52 -0600

    Agregando menu

--- gestorContactos.py
+++ gestorContactos.py
@@ -1,10 +1,11 @@
 import sqlite3
+import sys
 
 conection = sqlite3.connect("contactos")
 pointer = conection.cursor()
 
 #Crear tabla
-pointer.execute("CREATE TABLE CONTACTOS (NOMBRE_CONTACTO VARCHAR (50), NUMERO_CONTACTO INTEGER)")
+#pointer.execute("CREATE TABLE CONTACTOS (NOMBRE_CONTACTO VARCHAR (50), NUMERO_CONTACTO INTEGER)")
 
 
 def agregarContacto():
@@ -16,9 +17,19 @@ def eliminarContacto():
 def verContacto():
     pass
 
-def main():
-    pass
+menu = """
+Bienvenido a tu gestor de contactos, selecciona una opcion para ejecutar:
 
+    1 - Agregar contacto
+    2 - Eliminar contacto
+    3 - Ver contacto
+    4 - Ver TODOS los contactos
+    5 - Salir
+"""
+
+def main():
+    print(menu)
+    
 
 
 conection.close()
